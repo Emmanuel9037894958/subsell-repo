836fc6b45d900bc08e10f87a614d4f2b8300a1ae
Emmanuel9037894958 <emmanuelamamchukwu@gmail.com>
2026-08-17 02:21:08 +0100

    Fix home page build

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,8 +1,8 @@
 "use client";
 
 import Link from "next/link";
-import Navbar from "../components/Navbar";
-import Testimonials from "../components/Testimonials";
+import Navbar from "../../components/Navbar";
+import Testimonials from "../../components/Testimonials";
 import { MessageSquare } from "lucide-react";
 
 export default function Home() {
